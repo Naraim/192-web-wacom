a86277708a0653f46ae2091a981296af2fc57fd3
Marian Q <mariansofia1999@hotmail.com>
2019-09-08 19:02:39 -0500

    Añado css (Sin footer)

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,36 @@
+function handleLoad(){
+  //Gallery :D
+  var button = document.querySelector('.gallery__button');
+  var slider = document.querySelector('.gallery');
+  var tape = slider.querySelector('.gallery__tape');
+  var count = 0;
 
+  var first = tape.querySelector('.gallery__item');
+  var newLast = document.createElement('img');
+  newLast.setAttribute('src', first.getAttribute('src'));
+  newLast.classList.add('gallery__item');
+  tape.append(newLast);
+
+  function handleLast(){
+    tape.classList.add('gallery__tape--inactive');
+    tape.style.transform = 'translate(0px,0px)';
+  }
+
+  function handleClick(event){
+    count++;
+    if(count == 1){
+      tape.classList.remove('gallery__tape--inactive');
+    }
+
+    var mov = tape.offsetWidth * -1 * count;
+    tape.style.transform = 'translate(' + mov + 'px, 0px)';
+    if(count > tape.childElementCount -2){
+      setTimeout(handleLast,300);
+      count = 0;
+    }
+  }
+
+  button.addEventListener('click', handleClick);
+}
+
+window.addEventListener('load', handleLoad);
