7e7cf493c805c96eccbaa3e2b8b77842d1c801be
Marian Q <mariansofia1999@hotmail.com>
2019-09-11 23:21:45 -0500

    Añado galería (Funcional y carrusel)

--- scripts/main.js
+++ scripts/main.js
@@ -6,25 +6,64 @@ function handleLoad(){
   var tape = slider.querySelector('.gallery__tape');
   var count = 0;
 
+  var firstImg = document.querySelector('.gallery_image');
+  var lastItem = document.querySelector('.gallery__item:nth-of-type(6)'); //Aquí seleccionamos al item #6 de .gallery__item
+  console.log(lastItem);
+  var lastImg = lastItem.querySelector('.gallery_image');
+
+// Creo el último item COPIA del primero
+  var newLast = document.createElement('div');
+  newLast.classList.add('gallery__item');
+  var newLastImg = document.createElement('img');
+  newLastImg.setAttribute('src', firstImg.getAttribute('src'));
+  newLastImg.classList.add('gallery_image');
+
+  // Añado el div y el img a la cinta de imágenes. Los VINCULO :3
+  newLast.appendChild(newLastImg);
+  tape.appendChild(newLast); //Inserta al final de los hijos
+
+// Creo el primer Item COPIA del último
+  var newFirst = document.createElement('div');
+  newFirst.classList.add('gallery__item');
+  var newFirstImg = document.createElement('img');
+  newFirstImg.setAttribute('src', lastImg.getAttribute('src'));
+  newFirstImg.classList.add('gallery_image');
+
+  newFirst.appendChild(newFirstImg);
+  tape.insertBefore(newFirst, tape.firstChild); //(elElementoAPoner, Punto de Referencia (Si es el primer item, lo pone antes de ese item :v)
+
   function handleLast(){
     tape.classList.add('gallery__tape--inactive');
-    tape.style.transform = 'translate(0px,0px)';
+    tape.style.transform = 'translate(-80vw,0px)';
+    count = 1;
+  }
+
+  function handleFirst(){
+    tape.classList.add('gallery__tape--inactive');
+    tape.style.transform = 'translate(-480vw,0px)';
+    count = 6;
   }
 
   function handleClick(event){
       if(event.srcElement.classList.contains('btnR')){
-        if(count < tape.childElementCount - 1) {
           count++;
-        }
-
-      }else if(event.srcElement.classList.contains('btnL')){
-        if(count > 0) {
+      }else if(event.srcElement.classList.contains('btnL') && count > 0){
           count--;
-        }
-
       }
       console.log(count);
       tape.style.transform = 'translate('+ (-1*80*count) +'vw, 0)';
+
+      if(count == 2 || count == 0 || count == 5 || count == 7) {
+        tape.classList.remove('gallery__tape--inactive');
+      }
+
+      if(count > tape.childElementCount - 2){
+          setTimeout(handleLast, 300);
+      }
+
+      if(count < 1) {
+        setTimeout(handleFirst, 300);
+      }
   }
 
   buttonR.addEventListener('click', handleClick);
